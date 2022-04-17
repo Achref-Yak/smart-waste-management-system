ae62120b95b39e30d236e8d6673a73baebf5c9c0
Achref <achref.yakdhane@esprit.tn>
2022-04-17 20:54:27 +0100

    real time

--- frontservice/src/app/components/map/map.component.ts
+++ frontservice/src/app/components/map/map.component.ts
@@ -1,7 +1,9 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import * as L from 'leaflet';
 import * as geojson from 'geojson';
 import { TrashService } from 'src/app/services/Trash.service';
+import { WebsocketService } from 'src/app/services/websocketservice.service';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,6 +11,7 @@ import { TrashService } from 'src/app/services/Trash.service';
 })
 export class MapComponent implements AfterViewInit {
   private map;
+  subject = webSocket("wss://21t9ayt5cf.execute-api.us-west-2.amazonaws.com/production");
   sensorsData
   markers: Array<{
     id: string;
@@ -18,7 +21,7 @@ export class MapComponent implements AfterViewInit {
   }> = []
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
+      center: [39.8282, -98.5795],
       zoom: 3
     });
 
@@ -29,8 +32,8 @@ export class MapComponent implements AfterViewInit {
     });
 
     tiles.addTo(this.map);
-    
- 
+
+
 
 
 
@@ -38,67 +41,85 @@ export class MapComponent implements AfterViewInit {
 
   constructor(private trashService: TrashService) { }
 
-  getAllAWStrash()
-  {
+  getAllAWStrash() {
+
+
+  
     this.trashService.getAwsTrash().subscribe(data => {
       console.log(data);
       this.sensorsData = data;
-      
-let point = {
-  "id": "9",
-  "lat": 10.1815,
-  "lon": 36.8065,
-  "message": "level: high"
-}
- 
- 
 
-this.markers.push(point);
- 
-
-var i=0;
-console.log(this.sensorsData)
-
-this.markers.forEach(element => {
- 
-  if(element.id==this.sensorsData[i].id)
-  {
-    const message = "distance: " + this.sensorsData[i].distance + "</br> gaz : "  + this.sensorsData[i].gaz + "</br> " ;
-    element.message = message
-
-  }
-  i++
-
-});
-
- // Add custom icon
- var icon = L.icon({
-  iconUrl: 'assets/marker-icon-2x.png',
- 
-  iconSize: [30, 30], 
- 
- 
-});
-this.markers.forEach(element => {
-
-  var geojsonPoint: geojson.Point = {
-    type: 'Point',
-    coordinates: [element.lat,element.lon],
-  };
-  var marker = L.geoJSON(geojsonPoint, {
-    pointToLayer: (point,latlon)=> {
-      return L.marker(latlon, {icon: icon})
-  }
-  });
-  //Add popup message
-  marker.bindPopup(element.message);
-  marker.addTo(this.map);
-
+      let point = {
+        "id": "9",
+        "lat": 10.1815,
+        "lon": 36.8065,
+        "message": "level: high"
+      }
   
-});
+  
+  
+      this.markers.push(point);
+      this.setMarkers();
+
+    });
+
+    this.subject.subscribe(
+      msg => {
+        console.log(msg);
+        this.sensorsData = []
+        this.sensorsData.push(msg)
+        this.setMarkers()
+
+      }, // Called whenever there is a message from the server.
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
+  }
+
+
+  setMarkers() {
+
+    var i = 0;
+    console.log(this.sensorsData)
+
+    this.markers.forEach(element => {
+      console.log(this.sensorsData);
+
+      if (element.id == this.sensorsData[i].id) {
+        const message = "distance: " + this.sensorsData[i].distance + "</br> gaz : " + this.sensorsData[i].gaz + "</br> ";
+        element.message = message
+
+      }
+      i++
+
+    });
+
+    // Add custom icon
+    var icon = L.icon({
+      iconUrl: 'assets/marker-icon-2x.png',
+
+      iconSize: [30, 30],
+
+
+    });
+    this.markers.forEach(element => {
+
+      var geojsonPoint: geojson.Point = {
+        type: 'Point',
+        coordinates: [element.lat, element.lon],
+      };
+      var marker = L.geoJSON(geojsonPoint, {
+        pointToLayer: (point, latlon) => {
+          return L.marker(latlon, { icon: icon })
+        }
+      });
+      //Add popup message
+      marker.bindPopup(element.message);
+      marker.addTo(this.map);
+
+
     });
   }
-
   ngAfterViewInit(): void {
     this.getAllAWStrash();
     this.initMap();
